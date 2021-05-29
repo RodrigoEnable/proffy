3b15fd4b69395875588a8ae68d2937e0953c92a4
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:08:27 +0100

    Update api.ts

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.135:3333', // aqui utilizamos o ip de conexão no bundle do expo, apenas trocamos 'expo para http' e a porta para '3333'
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
