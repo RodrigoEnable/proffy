63350b8cb25a762c7402747fe69212baae6288f2
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:29:15 +0100

    Update 03_create_connection.ts

--- server/src/database/migrations/03_create_connection.ts
+++ server/src/database/migrations/03_create_connection.ts
@@ -1,15 +1,13 @@
 import Knex from 'knex';
 
-// os métodos aqui precisam ter exatamente o mesmo nome, up e down
-
 export async function up(knex: Knex) {
-  return knex.schema.createTable('connections', table => { // criamos a tabela do bd de nome connections
-    table.increments('id').primary(); // criamos o primeiro campo no bd, de nome id
-    table.integer('user_id').notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE'); // aqui temos um exemplo de relacionamento entre campos no knex
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary();
+    table.integer('user_id').notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP')).notNullable();
   });
 }
 
-export async function down(knex: Knex) { // deletamos a tabela
+export async function down(knex: Knex) {
   return knex.schema.dropTable('connections');
 }
