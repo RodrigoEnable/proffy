5c46f6baa3e4b552f67445170a382aa11fc26cca
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:43:58 +0100

    Update index.tsx

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,16 +1,14 @@
-import React, {InputHTMLAttributes} from 'react'; // InputHTMLAttributes fornece todos os atributos padrão que um elemento input pode receber
+import React, {InputHTMLAttributes} from 'react';
 import './styles.css';
 
-interface InputProps extends InputHTMLAttributes <HTMLInputElement> { // extendemos a interface InputProps a interface InputHTMLAttributes e definimos que queremos somente os atributos padrão do input
+interface InputProps extends InputHTMLAttributes <HTMLInputElement> {
   name: string;
   label: string;
 }
 
 const Input: React.FunctionComponent <InputProps> = ({label, name, ...rest}) => {
-  // fazemos uma desestruturação de props e pegamos label, name e repassamos os demais atributos padrão do input pelo rest operator
   return (
     <div className="input-block">
-      {/* no react utilizamos htmlFor ao invés de for */}
       <label htmlFor={name}>{label}</label>
       <input type="text" className={name} {...rest}/>
     </div>
