436a5a05e34a90e9d41dcd5c4f668c26360b94e9
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:34:49 +0100

    Update index.tsx

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'; // permite trabalhar com a biblioteca react
-import ReactDOM from 'react-dom'; // permite o react trabalhar com a dom do navegador
-import App from './App'; // componente
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
