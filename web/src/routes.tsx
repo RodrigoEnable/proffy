ca65050030bf4d8860a917448375fe9f886d7e4e
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:34:12 +0100

    Update routes.tsx

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'; // caso o projeto utilize typescript, é necessário instalar outro módulo, o @types/react-router-dom
+import {BrowserRouter, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
@@ -8,17 +8,17 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <Route 
-      path="/" // caminho para a página principal, a url ou rota de entrada, colocamos apenas a barra nesse caso
-      exact // a rota de entrada precisa dessa propriedade para ser acessada exclusivamente se o path for uma "/"
-      component={Landing} // o componente que será exibido ao acessar a rota
+      path="/"
+      exact
+      component={Landing}
       />
       <Route 
-      path="/study" // caminho para outra página
-      component={TeacherList} // o componente que será exibido ao acessar a rota
+      path="/study"
+      component={TeacherList}
       />
       <Route 
-      path="/give-classes" // caminho para outra página
-      component={TeacherForm} // o componente que será exibido ao acessar a rota
+      path="/give-classes"
+      component={TeacherForm}
       />
     </BrowserRouter>
   );
