cb7b32f793ce4fdcf02b3bfcf2e7aa030a710f20
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:51:04 +0100

    Update index.tsx

--- web/src/components/Textarea/index.tsx
+++ web/src/components/Textarea/index.tsx
@@ -1,16 +1,14 @@
-import React, {TextareaHTMLAttributes} from 'react'; // TextareaHTMLAttributes fornece todos os atributos padrão que um elemento textarea pode receber
+import React, {TextareaHTMLAttributes} from 'react';
 import './styles.css';
 
-interface TextareaProps extends TextareaHTMLAttributes <HTMLTextAreaElement> { // extendemos a interface TextareaProps a interface TextareaHTMLAttributes e definimos que queremos somente os atributos padrão do textarea
+interface TextareaProps extends TextareaHTMLAttributes <HTMLTextAreaElement> {
   name: string;
   label: string;
 }
 
 const Textarea: React.FunctionComponent <TextareaProps> = ({label, name, ...rest}) => {
-  // fazemos uma desestruturação de props e pegamos label, name e repassamos os demais atributos padrão do textarea pelo rest operator
   return (
     <div className="textarea-block">
-      {/* no react utilizamos htmlFor ao invés de for */}
       <label htmlFor={name}>{label}</label>
       <textarea className={name} {...rest}/>
     </div>
