ffadc80b251993e9bf3fbe00ed5eebdd0ebfb415
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:28:42 +0100

    Update 02_create_class_schelude.ts

--- server/src/database/migrations/02_create_class_schelude.ts
+++ server/src/database/migrations/02_create_class_schelude.ts
@@ -1,17 +1,15 @@
 import Knex from 'knex';
 
-// os métodos aqui precisam ter exatamente o mesmo nome, up e down
-
 export async function up(knex: Knex) {
-  return knex.schema.createTable('class_schedule', table => { // criamos a tabela do bd de nome class_schedule
-    table.increments('id').primary(); // criamos o primeiro campo no bd, de nome id
+  return knex.schema.createTable('class_schedule', table => {
+    table.increments('id').primary();
     table.integer('week_day').notNullable();
     table.integer('from').notNullable();
     table.integer('to').notNullable();
-    table.integer('class_id').notNullable().references('id').inTable('classes').onUpdate('CASCADE').onDelete('CASCADE'); // aqui temos um exemplo de relacionamento entre campos no knex
+    table.integer('class_id').notNullable().references('id').inTable('classes').onUpdate('CASCADE').onDelete('CASCADE');
   });
 }
 
-export async function down(knex: Knex) { // deletamos a tabela
+export async function down(knex: Knex) {
   return knex.schema.dropTable('class_schedule');
 }
