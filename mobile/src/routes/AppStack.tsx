10a9b278f66a1dbeca9445a80df52c2282423b1d
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-05-29 18:09:16 +0100

    Update AppStack.ts

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'; // importamos NavigationContainer, componente que deve ficar por volta de todas as rotas
-import {createStackNavigator} from '@react-navigation/stack'; // importamos createStackNavigator
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
 import StudyTab from './StudyTab';
@@ -14,7 +14,7 @@ const AppStack = () => {
       <Navigator screenOptions={{headerShown: false}}>
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
-        <Screen name="StudyTab" component={StudyTab} /* importamos a navegação por abas para dentro da nossa navegação por pilha */ />
+        <Screen name="StudyTab" component={StudyTab} />
       </Navigator>
     </NavigationContainer>
   )
